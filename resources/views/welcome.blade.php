b0bf4b23e3b5e87ad94ce3739cd6d914b0963e05
suryapramana <suryaadirpl21@smkn2tabanan.sch.id>
2023-11-08 09:51:13 +0800

    frist commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+      @font-face{
+        font-family: "sprak";
+        src: url("sprak.ttf");
+      }
+      @font-face {
+        font-family: "Samurai";
+        src: url("samu.ttf");
+      }
+      body {
+        background-image: url("warna.png");
+      
+      }
+      .text-blue {
+        font-family: "Samurai";
+        color: saddlebrown;
+        font-size: 30px;
+      }
+      h1{
+        padding-top: 2%;
+        font-family: "Samurai";
+        color: saddlebrown;
+        font-size: 55px;
+      }
+      
+      .navbar {
+        font-family: "Samurai";
+        color:black;`
+        font-size: 80px;
+      }
+      .img p {
+        font-family: "sprak";
+      }
+      .img1 p {
+        color: saddlebrown;
+        font-size: 20px;
+        text-align: center;
+      }
+
+      .img2 p {
+        color: saddlebrown;
+        font-size: 20px;
+        text-align: center;
+      }
+
+      .img3 p {
+        color: saddlebrown;
+        font-size: 20px;
+        text-align: center;
+      }
+    </style>
+
+    <title>Document</title>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg bg-dark bg-body-tertiary">
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <a class="nav-link" style="font-size: 25px" href="https://api.whatsapp.com/send/?phone=%2B6285738789375&text&type=phone_number&app_absent=0">PEMESANAN</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+</nav>
+
+   <center>
+    <div class="h1l">
+      <h1>Es Lilin</h1>
+    </div></center>
+    <div style="display:flex; justify-content:center;">
+      <img src="loogo.png.png" alt="loogo" style="width:400px; height:auto;">
+    </div>
+   
+  
+   <div>
+    <center><p class="text-blue">DI HARI-HARI YANG PANAS SEPERTI INI</p></center>
+    <center><p class="text-blue">SEBAIKNYA ANDA MENIKMATI ES LILIN KAMI</p></center>
+    <center><p class="text-blue">DI JAMIN MEBUAT DAHAGA HAUS ANDA MENGHILANG</p></center>
+   </div>
+   
+  <div class="img" style="display: flex; justify-content:space-around">
+    <div class="img1">
+      <img src="es coklat.png" alt="" width="400" height="auto">
+      <p>ES LILIN COKLAT</p>
+      <p>Rp1.500.00</p>
+    </div>
+    <div class="img2">
+      <img src="esalpukat.png" alt="" width="400" height="auto">
+      <p>ES LILIN ALPUKAT</p>
+      <p>Rp1.500.00</p>
+    </div>
+    <div class="img3">
+      <img src="kacangh.png" alt="" width="400" height="auto">
+      <p>ES LILIN KACANG HIJAU</p>
+      <p>Rp1.500.00</p>
+    </div>
+  </div>
+   
+</body>
+</html>
